43dc611b1ca565333d3527b2e1d9c1762a70a8e6
nginaldi <nginaldi@gmail.com>
2025-07-06 15:27:12 -0400

    command line

--- Sniffer.c
+++ Sniffer.c
@@ -46,6 +46,71 @@ int main(int argc, char ** argv){
    if(sockfd<0){
     exit_with_error("FAILED TO CREATE RAW SOCKET");
    }
-   //29.37
+   while(1){
+    static struct option long_options[]={
+        {"sip", required_argument,NULL,'s'},
+        {"dip", required_argument,NULL,'d'},
+        {"sport", required_argument,NULL,'p'},
+        {"dort", required_argument,NULL,'o'},
+        {"sif", required_argument,NULL,'i'},
+        {"dif", required_argument,NULL,'g'},
+        {"logfile", required_argument,NULL,'f'},
+        {"tcp", no_argument,NULL,'t'},
+        {"udp", no_argument,NULL,'u'},
+        {0,0,0,0}
+    };
+    c = getopt_long(argc,argc,"tus:d:p:o:i:g:f:",long_options,NULL); //sets optarg
+    if(c==-1){
+        break;
+    }
+    switch(c){
+        case 't':
+            packet_filter.t_protocol=IPPROTO_TCP;
+            break;
+        case 'u':
+            packet_filter.t_protocol=IPPROTO_UDP;
+            break;
+        case 'p':
+            packet_filter.source_port=atoi(optarg);
+            break;
+        case 'o':
+            packet_filter.dest_port=atoi(optarg);
+            break;
+        case 's':
+            packet_filter.source_ip=optarg;
+            break;
+        case 'd':
+            packet_filter.dest_ip=optarg;
+            break;
+        case 'i':
+            packet_filter.source_if_name=optarg;
+            break;
+        case 'g':
+            packet_filter.dest_if_name=optarg;
+            break;
+        case 'f':
+            strcpy(log,optarg);
+            break;
+        default:
+            abort();
+    }
+   }
+   printf("t_protocal: %d\n", packet_filter.t_protocal);
+   printf("source port: %d\n", packet_filter.source_port);
+   printf("destination port: %d\n", packet_filter.dest_port);
+   printf("source ip: %s\n", packet_filter.source_ip);
+   printf("destination ip: %s\n", packet_filter.dest_ip);
+   printf("source interface: %s\n", packet_filter.source_if_name);
+   printf("destination interface: %s\n", packet_filter.dest_if_name);
+   printf("log file %s\n", log);
+
+   if(strlen(log)==0){
+    strcopy(log, "sniffer_log.txt");
+   }
+   log_file=fopen(log,"w");
+   if(!log_file){
+    exit_with_error("FAILED TO OPEN LOG FILE");
+   }
+   //37.56
     return 0;
 }
